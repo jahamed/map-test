9d4449c7a583d01ea8e45b0c01216a95022fd391
Javed Ahamed <javed.b.ahamed@gmail.com>
2019-11-07 15:09:12 +0800

    learning sqlalchemy

--- app.py
+++ app.py
@@ -2,11 +2,23 @@
 
 from flask import Flask
 from flask import render_template
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,
 			template_folder="templates",
             static_folder="static")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schools.sqlite3'
+db = SQLAlchemy(app)
+
+db.Model.metadata.reflect(db.engine)
+
+class School(db.Model):
+	__tablename__ = 'schools-geocoded'
+	__table_args__ = { 'extend_existing': True }
+	LOC_CODE = db.Column(db.Text, primary_key=True)   
+
+#####
 
 @app.route("/ping")
 def sanity_check():
@@ -14,6 +26,15 @@ def sanity_check():
 
 @app.route("/")
 def homepage():
+	print("Total number of schools is", School.query.count())
+
+	school = School.query.filter_by(LOC_CODE='X270').first()
+	print("School's name is", school.SCHOOLNAME)
+
+	zip_schools = School.query.filter_by(ZIP='10466').all()
+	for zip_school in zip_schools:
+  		print(zip_school.SCHOOLNAME)
+
 	return render_template("index.html")
 
 if __name__ == '__main__':
