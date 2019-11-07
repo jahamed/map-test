83f8c45c13d91be93a7d8ecc9fe43ca04749b56f
Javed Ahamed <javed.b.ahamed@gmail.com>
2019-11-07 14:11:47 +0800

    working on school map tutorial

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+
 from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import flask_sqlalchemy
 
 app = Flask(__name__,
 			template_folder="templates",
@@ -8,5 +12,9 @@ app = Flask(__name__,
 def sanity_check():
 	return "pong"
 
+@app.route("/")
+def homepage():
+	return render_template("index.html")
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, port=5000)
